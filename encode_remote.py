6c543d378884328e23e7614e12ffea8cb9987bd9
WG.sss <wanggao.uni@gamil.com>
2024-03-07 14:22:13 +0800

    feat: add encoding script to deploy onto remote server

---
+++ encode_remote.py
@@ -0,0 +1,28 @@
+import csv
+import os
+from get_inference import *
+
+data_paths = './demo/split_dataset_paths.csv'
+# 读取 CSV 文件
+train_paths = []
+val_paths = []
+test_paths = []
+
+with open(data_paths, 'r', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        train_paths.append(row['train'])
+        val_paths.append(row['valid'])
+        test_paths.append(row['test'])
+
+val_paths = [val_dir for val_dir in val_paths if val_dir != '']
+test_paths = [test_dir for test_dir in test_paths if test_dir != '']
+
+checkpoint_path = '../checkpoint/checkpoint40-9.pth.tar'
+
+h5_data_paths = train_paths[:20] + val_paths[:20] + test_paths[:20]
+print(h5_data_paths)
+for h5_path in h5_data_paths[:20]:
+    parent_dir = os.path.dirname(h5_path) + '/'
+    get_inference(data_path=h5_path, z_dir=parent_dir, resume_file=checkpoint_path,
+                  img_dir=parent_dir, mode='encode')
